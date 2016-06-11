1556793063e108da01bf41583fe76a7d91f73f32
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-06-11 23:26:44 +0200

    Updated index

--- src/index.php
+++ src/index.php
@@ -1,1 +1,25 @@
 <?php
+
+require_once 'APIController.php';
+
+
+header('Content-Type: text/html; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+
+if (isset($_SERVER['PATH_INFO'])) {
+    $request_method = $_SERVER['REQUEST_METHOD'];   // HTTP request method
+    $path = explode('/', trim($_SERVER['PATH_INFO']));   // URI
+    $body = json_decode(file_get_contents('php://input'), true);    // JSON
+
+    $response = null;
+
+    if (json_last_error() == JSON_ERROR_NONE) {
+        $controller = new APIController($request_method, $path, $body);
+        $response = $controller->getResult();
+    } else {
+        // TODO: return error
+    }
+
+    header(':', true, $response->getStatusCode());
+    echo $response->getResponse();
+}
