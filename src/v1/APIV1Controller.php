3cb97544c208ba0892d20bcf444958e2e32b0afe
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-06-13 22:55:50 +0200

    Added APIV1Controller

--- src/v1/APIV1Controller.php
+++ src/v1/APIV1Controller.php
@@ -1,6 +1,21 @@
 <?php
 
-class APIV1Controller
-{
+require_once __DIR__ . '/response/Response.php';
 
+class APIV1Controller implements ControllerInterface
+{
+    /**
+     * APIV1Controller constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return Response
+     */
+    public function getResponse()
+    {
+        return new Response('v1');
+    }
 }
