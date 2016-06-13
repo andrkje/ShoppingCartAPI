de7fc1373850dd024f4049c9e06466eb19715ca6
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-06-13 23:18:25 +0200

    Removed first (empty) element of path, changed to getresoinse()

--- src/index.php
+++ src/index.php
@@ -2,22 +2,21 @@
 
 require_once 'APIController.php';
 
-
 header('Content-Type: text/html; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
 if (isset($_SERVER['PATH_INFO'])) {
     $request_method = $_SERVER['REQUEST_METHOD'];   // HTTP request method
     $path = explode('/', trim($_SERVER['PATH_INFO']));   // URI
+    array_shift($path);
     $body = json_decode(file_get_contents('php://input'), true);    // JSON
 
     $response = null;
 
     if (json_last_error() == JSON_ERROR_NONE) {
         $controller = new APIController($request_method, $path, $body);
-        $response = $controller->getResult();
+        $response = $controller->getResponse();
     } else {
-        // TODO: return error
     }
 
     header(':', true, $response->getStatusCode());
