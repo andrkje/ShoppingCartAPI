f2fdc431f1fc1fd2fd9cfc201ff765565d005f27
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-06-13 23:10:42 +0200

    Added message to InvalidPahtError

--- src/v1/response/errors/InvalidPathError.php
+++ src/v1/response/errors/InvalidPathError.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../HTTPStatusCodes.php';
+require_once __DIR__ . '/../ErrorResponse.php';
 
 class InvalidPathError extends ErrorResponse
 {
@@ -8,10 +9,12 @@ class InvalidPathError extends ErrorResponse
      * InvalidPathError constructor.
      * @param $path
      */
-    public function __construct($path)
+    public function __construct($path, $request_method)
     {
         $title = 'Invalid path error';
-        parent::__construct($title, $path, HTTPStatusCodes::NOT_FOUND);
+        $string_path = '\"/' . implode("/", $path) . '\"';  // TODO: Print prettier
+        $message = "The request method $request_method is not valid to the path: $string_path";
+        parent::__construct($title, $message, HTTPStatusCodes::NOT_FOUND);
     }
 
 }
