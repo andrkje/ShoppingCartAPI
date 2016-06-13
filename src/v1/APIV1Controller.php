4d5290e83702813272154a392823d344efbdd1ce
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-06-13 23:16:19 +0200

    Added arguments to constructor

--- src/v1/APIV1Controller.php
+++ src/v1/APIV1Controller.php
@@ -7,7 +7,7 @@ class APIV1Controller implements ControllerInterface
     /**
      * APIV1Controller constructor.
      */
-    public function __construct()
+    public function __construct($request_method, $path, $body)
     {
     }
 
